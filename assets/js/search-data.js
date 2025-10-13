7f93b8788aa576ccc0393088d90029f71f5f0349
cakiki <christopher.akiki@gmail.com>
2025-10-13 12:05:59 +0000

    Deploying to gh-pages from @ cakiki/govtech-25-ctf-writeup@305d2bb910bf751870fd41016f6970da547d8284 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/govtech-25-ctf-writeup/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-challenges",
+          title: "Challenges",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
